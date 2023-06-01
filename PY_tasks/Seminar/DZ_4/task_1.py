a57ab5f87b62ed9f48272efe6c55ac038f69c0dd
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-06-01 09:03:09 +0300

    task 1 done

--- PY_tasks/Seminar/DZ_4/task_1.py
+++ PY_tasks/Seminar/DZ_4/task_1.py
@@ -6,4 +6,20 @@
 # n — кол-во элементов первого множества.
 # m — кол-во элементов второго множества.
 # Затем пользователь вводит сами элементы множеств.
+#
 
+first_len = int(input("Enter number of length for the first set: "))
+second_len = int(input("Enter number of length for the second set: "))
+first_set = set()
+second_set = set()
+for i in range(first_len):
+    first_set.add(int(input(f"Enter {i+1} element for the first set: ")))
+
+for i in range(second_len):
+    second_set.add(int(input(f"Enter {i+1} element for the second set: ")))
+
+result_set = first_set.union(second_set).difference(first_set.intersection(second_set))
+print(sorted(first_set))
+print(sorted(second_set))
+print("Ihr Resultat aus einzelnen Zahlen in aufsteigender Reihenfolge:")
+print(sorted(result_set))
